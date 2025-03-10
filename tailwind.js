16e1be492348e9f12fa92b604b1e58906886790c
cheikh2025 <cherifadramediallo74@gmail.com>
2025-03-10 10:15:30 +0000

    Message de commit ici

---
+++ tailwind.js
@@ -0,0 +1,43 @@
+tailwind.config = {    
+    theme: {       
+        extend: {
+            keyframes: {
+                scroll: {
+                    '0%': { transform: 'translateX(0)' },
+                    '100%': { transform: 'translateX(-50%)' },
+                  },
+
+            },
+            animation: {
+                scroll: 'scroll 30s linear infinite',
+              },
+            screen: {
+                
+                
+
+            },            
+            colors: {
+                'custom-color': '#DF6951',
+                'star': '#A4074D',
+                'learn': '#A4074D',
+                'bordure':'#F9FCFF',
+                'trusted':'#EBEFF6',
+                'button':'#F4F4F4',
+            },
+           
+            boxShadow: {
+                bs:"2px 10px 12px 12px rgba(0,0,0,5)"
+            },
+            borderWidth: {
+                bw:"8px" ,
+            },
+            borderColor: {
+                bc:"#996600"
+            },
+            borderRadius: {
+                brd: "40px",
+                brdf: "9999px",
+            }
+        }
+    }
+};
